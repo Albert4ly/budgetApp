9aa638b12c270a973933ea619cfbf5aefb82e1b7
Albert <a91614830@gmail.com>
2024-04-09 22:49:22 +0200

    refactor:remove-changeClass-func

--- js/addItems.js
+++ js/addItems.js
@@ -12,35 +12,23 @@ export class AddItems {
 
    listenerFunc() {
       selectTypeItemBtn.addEventListener('click', (e) => {  this.changeSelectTypeClass(e) });
-      addItemBtn.addEventListener('click', (e) => { this.checkingClass(e) });
       addItemBtn.addEventListener('click', (e) => { this.fillTemplateOfData(e) });
-   }
-
-   checkingClass(e) {
-      e.preventDefault();
-
-      if (selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--incomes")) {
-         this.typeItem = true;
-      }
-      if (selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--exponses")) {
-         this.typeItem = false;
-      }
    }
 
    fillTemplateOfData(e) {
       e.preventDefault();
       const itemInput = document.querySelector(".itemInput__budgCnt");
 
-      if (this.typeItem = false) {
-         const templateExponses = document.querySelector(".expensesTemplateCnt__budgCnt");
-         const clone2 = templateExponses.content.cloneNode(true);
+      if (!selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--incomes")) {
+         const templateExpenses = document.querySelector("#expensesTemplateCnt__budgCnt");
+         const clone2 = templateExpenses.content.cloneNode(true);
 
-         const nameItemExponses = clone2.querySelector(".itemExponses__budgCnt");
-         nameItemExponses.innerText = itemInput.value;
+         const nameItemExpenses = clone2.querySelector(".itemExpenses__budgCnt");
+         nameItemExpenses.innerText = itemInput.value;
          itemsListCnt.append(clone2);
       }
 
-      if (this.typeItem = true) {
+      if (selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--incomes")) {
          const templateIncomes = document.querySelector("#incomesTemplateCnt__budgCnt");
          const clone = templateIncomes.content.cloneNode(true);
 
@@ -48,7 +36,8 @@ export class AddItems {
          nameItemIncomes.innerText = itemInput.value;
          itemsListCnt.append(clone);
       }
-  }
+   }
+  
 
    changeSelectTypeClass(e) {
       e.preventDefault();
