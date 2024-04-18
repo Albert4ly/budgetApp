318806d615cb60b85e03e0695c726523a1fd6937
Albert <a91614830@gmail.com>
2024-04-18 02:04:42 +0200

    feature:create-CountBudget-class-and-added-listenerFunc

---
+++ js/countBudget.js
@@ -0,0 +1,12 @@
+// const amountInput = document.querySelector(".valueInput__budgCnt");
+const addItemBtn = document.querySelector(".addItemBtn__budgCnt");
+
+export class CountBudget {
+	constructor() { 
+		this.listenerFunc();
+	}
+	
+	listenerFunc() {
+		addItemBtn.addEventListener('click', () => { });
+	}
+}
