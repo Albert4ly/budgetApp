658fa7d0c9369b399d28f7bc956d7165142b0c06
Albert <a91614830@gmail.com>
2024-04-18 20:47:41 +0200

    feature:added-countAmount-allow-with-changeSelectTypeClass-func

--- js/countBudget.js
+++ js/countBudget.js
@@ -1,4 +1,4 @@
-// const amountInput = document.querySelector(".valueInput__budgCnt");
+const selectTypeItemBtn = document.querySelector(".selectionOfItemsTypeBtn__budgCnt");
 const addItemBtn = document.querySelector(".addItemBtn__budgCnt");
 
 export class CountBudget {
@@ -7,6 +7,38 @@ export class CountBudget {
 	}
 	
 	listenerFunc() {
-		addItemBtn.addEventListener('click', () => { });
+		selectTypeItemBtn.addEventListener('click', (e) => {  this.changeSelectTypeClass(e) });
+		addItemBtn.addEventListener('click', (e) => { this.countAmount(e) });
 	}
+
+	countAmount(e) {
+		if (!selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--incomes")) {
+			this.subtractionAmount();
+		}
+		if (selectTypeItemBtn.classList.contains("selectionOfItemsTypeBtn__budgCnt--incomes")) {
+			this.addingAmount();
+		}
+	}
+
+	addingAmount() {
+		const statusOfWallet = document.querySelector("span");
+		const amountValueInput = document.querySelector(".valueInput__budgCnt");
+
+		let amount = Number(statusOfWallet) + Number(amountValueInput.value);
+		statusOfWallet.innerText = amount;
+	}
+
+	subtractionAmount() {
+		const statusOfWallet = document.querySelector("span");
+		const amountValueInput = document.querySelector(".valueInput__budgCnt");
+
+		let amount = Number(statusOfWallet) - Number(amountValueInput.value);
+		statusOfWallet.innerText = amount;
+	}
+
+	changeSelectTypeClass(e) {
+      e.preventDefault();
+      selectTypeItemBtn.classList.toggle("selectionOfItemsTypeBtn__budgCnt--incomes");
+   }
+
 }
